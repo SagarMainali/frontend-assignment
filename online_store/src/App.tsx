4b5188159f7feecaf010fa82ac22b5f72d080f34
Sagar Mainali <sagarmainali78@gmail.com>
2023-07-20 23:22:36 +0545

    added navbar

--- online_store/src/App.tsx
+++ online_store/src/App.tsx
@@ -7,12 +7,12 @@ import { Home } from './Pages/Home'
 export default function App() {
 
   return (
-    <div className='app'>
+    <div className='max-w-[1500px] mx-auto'>
       <Routes>
         <Route path='/' element={<Layout />} >
           <Route index element={<Home />} />
           <Route path=':id' element={<ProductDetails />} />
-          <Route path='Cart' element={<Cart />} />
+          <Route path='cart' element={<Cart />} />
         </Route>
       </Routes>
     </div>
