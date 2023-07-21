5dbed3449729c95fca1d90579ecf977d9a765eb6
Sagar Mainali <sagarmainali78@gmail.com>
2023-07-22 01:47:08 +0545

    added similar products functionality

--- online_store/src/Components/ProductInCart.tsx
+++ online_store/src/Components/ProductInCart.tsx
@@ -12,7 +12,7 @@ export default function ProductInCart({ id, image, title, cartQuantity, price }:
                     <img src={image} alt='product-img' className="object-cover h-[6.5rem]" />
                </div>
 
-               <div className="flex flex-col gap-1">
+               <div className="flex flex-col gap-2">
                     <h2 className="leading-5 text-sm sm:text-base">{title} <button onClick={() => removeFromCart(id)} className="text-xs text-red-600 font-semibold bg-gray-200 px-2 rounded-lg">Remove</button></h2>
                     <div className="flex items-center gap-2">Quantity:
                          <button onClick={() => changeCartQuantity(id, 'decrement')} className="flex justify-center items-center bg-gray-200 rounded-full h-6 w-6 pb-1">-</button>
---
+++ online_store/src/Components/SimilarProducts.tsx
@@ -0,0 +1,33 @@
+import { useGlobalContext } from "../StateManagement/context"
+import { ProductType } from "../Types/type"
+import { NavLink } from "react-router-dom"
+import { useEffect, useState } from 'react'
+
+export function SimilarProducts({ category, id }: { category: string, id: number }) {
+
+     const { data, isLoading } = useGlobalContext()
+
+     const [similarProducts, setSimilarProducts] = useState<ProductType[] | undefined>([])
+
+     useEffect(() => {
+          const storeSimilarProducts = data?.filter(
+               (product: ProductType) => {
+                    return product.category === category && product.id !== id
+               }
+          )
+          setSimilarProducts(storeSimilarProducts)
+     }, [isLoading])
+
+     return (
+          similarProducts?.map(
+               (similarProduct: ProductType) => {
+                    return (
+                         <NavLink key={similarProduct.id} to={`/${similarProduct.id}`} className="flex flex-col gap-2 items-center text-sm p-3 border-2 border-slate-200 hover:bg-slate-50 group duration-300">
+                              <img src={similarProduct.image} alt="product-img" className="h-[7rem] object-cover group-hover:scale-[.90] duration-300" />
+                              <h2 className="group-hover:underline duration-300">{similarProduct.title}</h2>
+                         </NavLink>
+                    )
+               }
+          )
+     )
+}
