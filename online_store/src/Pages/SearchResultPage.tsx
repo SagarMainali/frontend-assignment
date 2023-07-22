d1fcc50d251b3e3c4be836cd1cd8c2199c62c1f2
Sagar Mainali <sagarmainali78@gmail.com>
2023-07-22 10:16:10 +0545

    added netlify routing configuration file and also handled unmatched search query

--- online_store/src/Pages/SearchResultPage.tsx
+++ online_store/src/Pages/SearchResultPage.tsx
@@ -22,22 +22,27 @@ export function SearchResultPage() {
      }, [isLoading, searchQuery])
 
      return (
-          <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
+          <div>
                {
                     isLoading
                          ? <LoadingHandler />
-                         : searchMatchedProducts
-                              ? searchMatchedProducts.map(
-                                   (searchMatchedProduct: ProductType) => {
-                                        return (
-                                             <NavLink key={searchMatchedProduct.id} to={`/products/${searchMatchedProduct.id}`} className="flex flex-col gap-2 items-center text-sm p-3 border-2 border-slate-200 hover:bg-slate-50 group duration-300">
-                                                  <img src={searchMatchedProduct.image} alt="product-img" className="h-[7rem] object-cover group-hover:scale-[.90] duration-300" />
-                                                  <h2 className="group-hover:underline duration-300">{searchMatchedProduct.title}</h2>
-                                             </NavLink>
+                         : <div className="grid md:grid-cols-4 sm:grid-cols-2 grid-cols-1 gap-4">
+                              {
+                                   searchMatchedProducts?.length === 0
+                                        ? <h2> We are really sorry to inform you that the product you are searching is not available at our store.🙏🏼😔</h2 >
+                                        : searchMatchedProducts?.map(
+                                             (searchMatchedProduct: ProductType) => {
+                                                  return (
+                                                       <NavLink key={searchMatchedProduct.id} to={`/products/${searchMatchedProduct.id}`} className="flex flex-col gap-2 items-center text-sm p-3 border-2 border-slate-200 hover:bg-slate-50 group duration-300">
+                                                            <img src={searchMatchedProduct.image} alt="product-img" className="h-[7rem] object-cover group-hover:scale-[.90] duration-300" />
+                                                            <h2 className="group-hover:underline duration-300">{searchMatchedProduct.title}</h2>
+                                                       </NavLink>
+                                                  )
+                                             }
                                         )
-                                   }
-                              )
-                              : <h2> We are really sorry to inform you that the product you are searching is not available at our store.🙏🏼😔</h2 >
+                              }
+                         </div>
+
                }
           </div >
      )
